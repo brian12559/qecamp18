c6a8912d3b48c6894cd4ca0edd4d0daa531bcec3
brian12559 <bmurray@redhat.com>
2019-05-15 12:31:53 -0400

    new locaators for other tools

--- locators/loginLocators.py
+++ locators/loginLocators.py
@@ -20,8 +20,32 @@ class LoginPageLocators(object):
     ERROR_MESSAGE = (By.ID, 'errorMessage')
     POLARION_LOGO = (By.XPATH,"//img[@class[contains(., 'polarion-Logo')]]")
     LOADING = (By.XPATH,"//img[@src[contains(., 'progress.gif')]]")
-                     
-  
+
+    #for xray
+    USERNAMEXRAY = (By.ID, 'login-form-username')
+    PASSWORDXRAY = (By.ID, 'login-form-password')
+    SUBMITXRAY = (By.ID, 'login-form-submit')
+    ISSUEKEYXRAY = (By.ID, 'raven-test-filter-contains-text')
+
+    #for CodeBeamer
+    USERNAMECB = (By.ID, 'user')
+    PASSWORDCB = (By.ID, 'password')
+    SUBMITCB = (By.ID, 'login-button')
+    ISSUEKEYCB = (By.ID, 'raven-test-filter-contains-text') #not done as can't login yet
+
+    # for TestLab..hmmm this might be tricky...looks like its embedded java so inspect element does not work
+    USERNAMETL = (By.ID, 'user')
+    PASSWORDTL = (By.ID, 'password')
+    SUBMITTL = (By.ID, 'login-button')
+    ISSUEKEYTL = (By.ID, 'raven-test-filter-contains-text')  # not done as can't login yet
+
+    # for PT
+    USERNAMEPT = (By.NAME, 'email')
+    PASSWORDPT = (By.NAME, 'password')
+    SUBMITPT = (By.NAME, 'submit') #submit type id Sign in
+    ISSUEKEYPT = (By.ID, 'raven-test-filter-contains-text')  # not done as can't login yet
+
+
 class LogoutPageLocators(object):
     GEARICONrec    = (By.CSS_SELECTOR, 'img.gwt-Image')  #what IDE recorded
     GEARICON    = (By.XPATH, '//img[@src[contains(., \'settings_off\')]]') #what I got from firebug
